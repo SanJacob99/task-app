096639d7e0e348fdfb57e47e49a1ec34b3de1d7c
JacobAmador <jacobo.amador@ahinko.com>
2024-05-04 11:29:58 -0600

    feat: navigation

---
+++ screens/HomeScreen.tsx
@@ -0,0 +1,47 @@
+import React from "react";
+import { StyleSheet, View, TouchableHighlight, Text, Alert } from "react-native";
+
+export default function App({ navigation }) {
+	const handleTasksPress = () => {
+		navigation.navigate("Tasks");
+	};
+
+	const handleListPress = () => {
+		navigation.navigate("List");
+	};
+
+	return (
+		<View style={styles.container}>
+			<TouchableHighlight style={[styles.buttonContainer]} onPress={handleTasksPress} underlayColor="#C5C5C5">
+				<Text style={styles.buttonText}>Tasks</Text>
+			</TouchableHighlight>
+			<TouchableHighlight style={[styles.buttonContainer]} onPress={handleListPress} underlayColor="#C5C5C5">
+				<Text style={styles.buttonText}>List</Text>
+			</TouchableHighlight>
+		</View>
+	);
+}
+
+const styles = StyleSheet.create({
+	container: {
+		flex: 1,
+		backgroundColor: "#fff",
+		alignItems: "center",
+		justifyContent: "center",
+		flexDirection: "column"
+	},
+	buttonContainer: {
+		marginVertical: 10,
+		width: "50%",
+		borderRadius: 20,
+		overflow: "hidden",
+		backgroundColor: "#800080",
+		alignItems: "center",
+		justifyContent: "center",
+		height: 40
+	},
+	buttonText: {
+		color: "#FFF",
+		fontSize: 16
+	}
+});
